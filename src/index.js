1c25c7f4b66b3d4f3c372144a1555bfaa339cd1f
@EkaHvo <ekasagalaeva@gmail.com>
2022-06-25 11:31:22 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(num) {
+    let result = "";
+
+    let ones = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    let tens = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
   
-}
+    let myHngr = Math.trunc(num / 100);
+    let myTens = num % 100;
+    let myOnes = num % 10;
+  
+    if(num === 0){
+      return 'zero';
+    }
+  
+    if(myHngr){
+      result += `${ones[myHngr - 1]} hundred `;
+    }
+    if(Math.trunc(myTens /10)){
+      if(myTens < 20){
+        result += `${ones[myTens - 1]}`;
+        return result;
+      } else {
+        result += `${tens[Math.trunc(myTens /10) - 2]} `;
+      }
+    }
+  
+    if(myOnes){
+      result += `${ones[myOnes - 1]}`;
+    }
+  
+    return result.trim();
+};
